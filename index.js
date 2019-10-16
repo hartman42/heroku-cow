792a5ddd8cbd4981ef201d126587f3e77a1dca13
Michael Hartman <michael.hartman@dunbararmored.com>
2019-10-16 19:46:28 -0400

    WIP

--- index.js
+++ index.js
@@ -5,14 +5,6 @@ const cors = require("cors");
 
 const app = express();
 
-const path = require("path");
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, "client/build")));
-// Anything that doesn't match the above, send back index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
-
 app.get("/api/cow/:say", cors(), async (req, res, next) => {
   try {
     const text = req.params.say;
@@ -35,4 +27,12 @@ app.get("/api/cow/", cors(), async (req, res, next) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`slice of fine on port ${PORT}`);
+});
+
+const path = require("path");
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "client/build")));
+// Anything that doesn't match the above, send back index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
